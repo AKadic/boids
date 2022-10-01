6f584e87804b29facb2a1e1eee1219882d1280f2
Akadic <akadic01@gmail.com>
2022-10-01 12:17:27 +0200

    Add macos arm64 build

--- engine/setup.py
+++ engine/setup.py
@@ -132,7 +132,7 @@ class CMakeBuild(build_ext):
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="engine_akadic01",
-    version="0.0.4",
+    version="0.0.5",
     ext_modules=[CMakeExtension("engine", "..")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
